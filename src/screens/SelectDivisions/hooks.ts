e6c741270b760b2ec3d640da895239853ecb6774
Eduardo Almeida <contato.eduardopex@hotmail.com>
2022-09-27 20:21:32 -0300

    chore: started to create firebase integration

--- src/screens/SelectDivisions/hooks.ts
+++ src/screens/SelectDivisions/hooks.ts
@@ -3,23 +3,85 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { DIVISIONS_LIST } from "./constants";
 import { CURRENT_DIVISION } from "../../constants/storage";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
+import {database} from "../../services/firebase";
 
 export const useSelectDivisions = () => {
-  const { navigate } = useNavigation<any>()
+  const { navigate } = useNavigation<any>();
   const [list, setList] = useState<any[]>([]);
 
   const getList = async () => setList(DIVISIONS_LIST);
 
   const setCurrentDivision = async (division: any) => {
     AsyncStorage.setItem(CURRENT_DIVISION, JSON.stringify(division));
-    
-    navigate('SelectTraining');
+
+    navigate("SelectTraining");
   };
 
   useEffect(() => {
     getList();
   }, []);
+
+
+  useEffect(async () => {
+
+database.ref("rules/division").set([
+  {
+    title: "A",
+    description: "Adicionar descrição da divisão do treino",
+    type: "division",
+    },
+    {
+      title: "AB",
+      description: "Adicionar descrição da divisão do treino",
+      type: "division",
+      },
+    {
+      title: "ABC",
+      description: "Adicionar descrição da divisão do treino",
+      type: "division",
+      },
+    {
+      title: "ABCD",
+      description: "Adicionar descrição da divisão do treino",
+      type: "division",
+      },
+])
+
+
+database.ref("rules/training").set([
+  {
+    title: "Posterior 1",
+    description: "Adicionar descrição da divisão do treino",
+    type: "division",
+    },
+    {
+      title: "Posterior 2",
+      description: "Adicionar descrição da divisão do treino",
+      type: "division",
+      },
+    {
+      title: "Posterior 3",
+      description: "Adicionar descrição da divisão do treino",
+      type: "division",
+      },
+    {
+      title: "Inferior 1",
+      description: "Adicionar descrição da divisão do treino",
+      type: "division",
+      },
+    {
+      title: "Inferior 2",
+      description: "Adicionar descrição da divisão do treino",
+      type: "division",
+      },
+    {
+      title: "Inferior 3",
+      description: "Adicionar descrição da divisão do treino",
+      type: "division",
+      },
+])
+  }, [])
 
   return {
     list,
---
+++ src/services/firebase/division.example.md
@@ -0,0 +1,5 @@
+database.ref('division').push({
+title: "A",
+description: "Adicionar descrição da divisão do treino",
+type: "division",
+});
