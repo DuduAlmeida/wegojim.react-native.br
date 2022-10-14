113cf34fab33c4d96bf57582f2ebc762cd129173
Eduardo Almeida <contato.eduardopex@hotmail.com>
2022-10-13 22:30:36 -0300

    chore: added Division's service.

--- src/services/divisions/index.ts
+++ src/services/divisions/index.ts
@@ -1,28 +1,28 @@
-import { DivisionProxy } from './types';
-import { Division, DivisionList } from './entities';
+import { DivisionProxy } from "./types";
+import { Division, DivisionList } from "./entities";
 import { database } from "services/firebase";
 
 const getOne = async ({ event_id }: any): Promise<Function> => {
-  const divisionRef = database.ref(`division/${event_id}`)
+  const divisionRef = database.ref(`division/${event_id}`);
 
-  const execute = (
-    getData = (_data: any): DivisionProxy => _data
-  ) => {
-    divisionRef.on('value', event => {
-      const databaseDivision = event.val()
-      const data = Division({ id: event_id, ...databaseDivision } ?? { id: event_id, })
+  const execute = (getData = (_data: DivisionProxy): any => _data) => {
+    divisionRef.on("value", (event) => {
+      const databaseDivision = event.val();
+      const data = Division(
+        { id: event_id, ...databaseDivision } ?? { id: event_id }
+      );
 
-      getData(data)
-    })
-  }
+      getData(data);
+    });
+  };
 
   return execute;
 };
 
-const getList = () => {
+const getList = async () => {
   const divisionRef = database.ref(`division`);
 
-  const execute = (getData = (_data: any): DivisionProxy[] => _data) => {
+  const execute = (getData = (_data: DivisionProxy[]): any => _data) => {
     divisionRef.on("value", (event) => {
       const databaseDivision = event.val();
       let data = DivisionList(databaseDivision ?? {});
