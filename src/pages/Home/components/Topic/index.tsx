4ddf0dc741c373f0cd952b304c5155070e2036ae
Eduardo Almeida <contato.eduardopex@hotmail.com>
2022-11-25 16:07:54 -0300

    style: topic's component.

--- src/pages/Home/components/Topic/index.tsx
+++ src/pages/Home/components/Topic/index.tsx
@@ -1,8 +1,9 @@
 import { TopicProps } from "./types";
 
+import "./Topic.scss";
+
 export const Topic = ({ text }: TopicProps) => (
   <li className="home-topic">
-    <div className="home-topic__description">SUGESTÃO DO DIA</div>
     <div className="home-topic__title">{text}</div>
   </li>
 );
